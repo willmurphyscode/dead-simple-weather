51f2b7a777360c891d4d4efd5277a38fd5c1c3b2
Will Murphy <willmurphyscode@gmail.com>
2017-05-04 07:47:26 -0400

    add zipcode support

--- test/command/fetch_temperature_by_zip_command_test.rb
+++ test/command/fetch_temperature_by_zip_command_test.rb
@@ -3,9 +3,21 @@ require 'test_helper'
 class FetchTemperatureByZipCommandTest < ActiveSupport::TestCase
 
   test '#execute' do
+    response = FetchTemperatureByZipCommand.execute('Alexandria, VA')
+    temp = Float(response.dig('list', 0, 'main', 'temp'))
+    assert temp.instance_of?(Float), 'expected a float'
+  end
+
+  test '#execute with a zip' do
     response = FetchTemperatureByZipCommand.execute('22314')
     temp = Float(response.dig('list', 0, 'main', 'temp'))
     assert temp.instance_of?(Float), 'expected a float'
   end
+
+  test '#patch_foo' do
+    assert FetchTemperatureByZipCommand.patch_foo, 'failed to patch'
+  end
+
+
 
 end
