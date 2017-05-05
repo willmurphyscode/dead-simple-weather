720c67226c098b244cd7f52eae733c7b1cd0d68d
Will Murphy <willmurphyscode@gmail.com>
2017-05-05 07:21:19 -0400

    prevent launch if api key not configure

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+raise 'no api key' if Rails.application.secrets[:open_weather_map_key].nil? || Rails.application.secrets[:open_weather_map_key].blank?
